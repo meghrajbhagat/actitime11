9928dd2558f675a37b77aafbd0909d9342e94f26
meghrajbhagat <125956515+meghrajbhagat@users.noreply.github.com>
2023-02-27 09:31:29 +0530

    Add files via upload

---
+++ Dragby1.java
@@ -0,0 +1,25 @@
+package actionclass;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Dragby1 {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://jqueryui.com/draggable/");
+		driver.switchTo().frame(0);
+		WebElement source = driver.findElement(By.id("draggable"));
+		Actions a=new Actions(driver);
+		a.dragAndDropBy(source, 80, 0).perform();
+	
+	}
+	
+
+}
